e3aa6be445bfc5d74a7b119b32a0f54cf8e37b94
ruchamahabal <ruchamahabal2@gmail.com>
2019-01-11 14:26:40 +0530

    about us

--- index.php
+++ index.php
@@ -19,8 +19,47 @@
     </div>
   </div>
 </section>
-
-
-
-
+<!--ABOUT US -->
+<section id="about-us-new">
+  <div id="about-us-mobile" class="wow fadeInUp">
+    <h1 class="heading"><center>ABOUT US</center></h1>
+    <div class="row" id="about-sk-mobile-content">
+      <h2 align=center>Placement Cell</h2>
+      <p>The PLACEMENT CELL is a student initiative which has evolved from the INTERNSHIP CELL and it comprised entirely of students. The main aim of the cell is to provide placements & internships to students. The PLACEMENT CELL consists of students from all the courses as well as teachers. Members of the PLACEMENT CELL work for the benefit of the students of the college. The PLACEMENT CELL of the college, had provided a good amount of placements as well as internships to all the students previously. </p>
+    </div>
+    <div class="row" >
+      <img id="about-sk-mobile-image" src="img\about-us-1.jpg">
+    </div>
+    <div class="row" id="about-txp-mobile-content">
+      <h2>Who We Are</h2>
+      <p>We are the PLACEMENT CELL committee which is entirely run by student’s body. And only 4 colleges in Mumbai have a student’s Placement body. Our team works selflessly for providing internship and Placement opportunity to our college students. Also we provide training to students for improving their skills. </p>
+    </div>
+    <div class="row">
+      <img id="about-txp-mobile-image" src="assets/images/txp-team.jpg">
+    </div>
+  </div>
+  <div id="about-us2">
+  <h1 class="heading"><center>ABOUT US</center></h1>
+  <section id="about-somaiya" class="wow fadeInUp">
+    <div class="row">
+        <div class="col content">
+          <h2>Who We Are</h2>
+          <p>We are the PLACEMENT CELL committee which is entirely run by student’s body. And only 4 colleges in Mumbai have a student’s Placement body. Our team works selflessly for providing internship and Placement opportunity to our college students. Also we provide training to students for improving their skills. </p>
+        </div>
+        <div class="col image-box">
+        </div>
+    </div>
+  </section>
+  <section id="about-txp" class="wow fadeInUp">
+    <div class="row">
+      <div class="col image-box">
+      </div>
+        <div class="col content">
+            <h2>Who We Are</h2>
+            <p>We are the PLACEMENT CELL committee which is entirely run by student’s body. And only 4 colleges in Mumbai have a student’s Placement body. Our team works selflessly for providing internship and Placement opportunity to our college students. Also we provide training to students for improving their skills. </p>
+        </div>
+    </div>
+  </section>
+</div>
+</section>
 <?php include 'footer.php'; ?>
