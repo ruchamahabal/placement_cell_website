ede93c0641860b1279e516f8b1fdf3f419d3142f
root <cybersagex@gmail.com>
2019-01-11 12:39:43 +0530

    some fixes in header section

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <?php include 'header.php'; ?>
 
 <section id="hero-section">
-  <div class="row container">
-    <div class="col">
-      <div id="typed-strings">
-      <p>Typed.js is a <strong>JavaScript</strong> library.</p>
-      <p>It <em>types</em> out sentences.</p>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <center>
+          <h1>S. K. Placement Cell</h1><br>
+          <div id="typed-strings">
+          <p>Typed.js is a <strong>JavaScript</strong> library.</p>
+          <p>It <em>types</em> out sentences.</p>
+          </div>
+          <span id="typed1"></span>
+      </center>
       </div>
-      <span id="typed1"></span>
-    </div>
-    <div class="col">
-
+      <div class="col">
+        <img src="img/placement_cell_logo.jpg" alt="Placement Cell logo">
+      </div>
     </div>
   </div>
-
 </section>
 
 
